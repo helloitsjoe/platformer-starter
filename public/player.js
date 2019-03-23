688a0bb2b7f39fa7e3c3628c5cabdbd3357fa1cf
helloitsjoe <jboyle617@gmail.com>
2019-03-23 12:47:10 -0400

    start controller

--- public/player.js
+++ public/player.js
@@ -3,11 +3,48 @@ const ctx = canvas.getContext('2d');
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
+const isIn = (pos, square) => {
+  const [x, y, size] = square;
+  console.log(`pos:`, pos);
+  console.log(`square:`, square);
+  return pos.x > x && pos.x < x + size && pos.y > y && pos.y < y + size;
+};
+
+const makeSquare = (x, y, size = SIZE) => [x * size, y * size, size, size];
+
+const SIZE = 50;
+
+const upButton = makeSquare(1, 0);
+const leftButton = makeSquare(0, 1);
+const rightButton = makeSquare(2, 1);
+const downButton = makeSquare(1, 2);
+
+const jumpButton = makeSquare(1.7, 0.3, SIZE * 2);
+
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+ctx.fillStyle = 'red';
+ctx.fillRect(...upButton);
+ctx.fillRect(...leftButton);
+ctx.fillRect(...rightButton);
+ctx.fillRect(...downButton);
+
+ctx.fillStyle = 'white';
+ctx.fillRect(...jumpButton);
+
 // socket.io script from player.html
 const socket = io();
 
 socket.on('connected', () => console.log('connected!'));
 
 canvas.addEventListener('click', e => {
-  socket.emit('tap');
+  const { clientX: x, clientY: y } = e;
+  console.log(`x:`, x);
+  console.log(`y`, y);
+  if (isIn({ x, y }, leftButton)) {
+    socket.emit('tap', 'left');
+  } else if (isIn({ x, y }, rightButton)) {
+    socket.emit('tap', 'right');
+  }
 });
--- server/gameServer.js
+++ server/gameServer.js
@@ -22,8 +22,8 @@ const createServer = (port = 3001) => {
   io.on('connection', socket => {
     console.log(`A new user connected!`);
     socket.emit('connected');
-    socket.on('tap', ({ x, y }) => {
-      console.log(`tapped: ${x}, ${y}`);
+    socket.on('tap', direction => {
+      console.log(`tapped: ${direction}`);
       socket.broadcast.emit('relay-tap');
     });
 
